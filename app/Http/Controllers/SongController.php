895e6c5a857570435a3cd0fe950531f20aa7fbe1
borisbrnkalak <borisbrnkalak@gmail.com>
2022-04-27 18:46:04 +0200

    Storing files into database

--- app/Http/Controllers/SongController.php
+++ app/Http/Controllers/SongController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AddSongRequest;
 use App\Models\Song;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use wapmorgan\Mp3Info\Mp3Info;
 
 class SongController extends Controller
 {
@@ -23,8 +26,16 @@ class SongController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(AddSongRequest $request)
     {
-        //
+        Song::create($request->except(['image', 'song']));
+        $song = $request->file('song');
+        $image = $request->file('image');
+        Storage::disk('public')->put('audio/' . $song->hashName(), $song);
+
+
+        $audio = new Mp3Info(Storage::disk('public')->path('audio/' . $song->hashName()));
+
+        Storage::disk('public')->put('images/', $image);
     }
 }
