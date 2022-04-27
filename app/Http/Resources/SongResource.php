980cbce6cad540379b27c57319565bb2e4546715
borisbrnkalak <borisbrnkalak@gmail.com>
2022-04-27 19:29:21 +0200

    Added id to SongResource

--- app/Http/Resources/SongResource.php
+++ app/Http/Resources/SongResource.php
@@ -15,6 +15,7 @@ class SongResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'author' => $this->author,
             'year' => $this->year,
