59f4e0434788d7b89256bf70bcdf2a69ca319236
borisbrnkalak <borisbrnkalak@gmail.com>
2022-04-27 11:26:49 +0200

    Added custom scrollbar

--- resources/js/components/SongsList/SongsList.jsx
+++ resources/js/components/SongsList/SongsList.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import SongsListItem from "./SongsListItem";
 import PropTypes from "prop-types";
+import { Scrollbar } from "react-scrollbars-custom";
 
 export default function SongsList(props) {
     return (
-        <div className="scrollbar overflow-y-scroll h-[calc(100%-100px)] pr-4">
-            <ul>
-                {props.songs.map((item) => {
-                    return (
-                        <SongsListItem
-                            key={item.id}
-                            name={item.name}
-                            author={item.author}
-                            year={item.year}
-                        />
-                    );
-                })}
-            </ul>
-        </div>
+        <Scrollbar
+            className="scrollbar"
+            style={{ height: `calc(100% - 100px)` }}
+        >
+            <div className="scrollbar h-[calc(100%-100px)] pr-4">
+                <ul>
+                    {props.songs.map((item) => {
+                        return (
+                            <SongsListItem
+                                key={item.id}
+                                name={item.name}
+                                author={item.author}
+                                year={item.year}
+                            />
+                        );
+                    })}
+                </ul>
+            </div>
+        </Scrollbar>
     );
 }
 
